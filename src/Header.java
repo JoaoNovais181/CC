f3a5efb5418bf246b77f161c6c823c88213f3a0c
JoaoNovais181 <jonasnovais1289@gmail.com>
2022-11-16 15:23:11 +0000

    coisas novas

--- src/Header.java
+++ src/Header.java
@@ -1,5 +1,43 @@
+import java.nio.ByteBuffer;
+import java.util.BitSet;
 
 public class Header
 {
-    private String MsgID, Flags,
+    private String MsgID, Flags;
+    private int responseCode;
+    private byte numberOfValues, numberOfAuthorities, numberOfExtraValues;
+
+    public Header(String MsgID, String Flags)
+    {
+        this.MsgID = MsgID;
+        this.Flags = Flags;
+        this.responseCode = 0;
+        this.numberOfValues = 0;
+        this.numberOfAuthorities = 0;
+        this.numberOfExtraValues = (byte) 255;
+    }
+
+    public Header(String MsgID, String Flags, int responseCode, byte numberOfValues, byte numberOfAuthorities, byte numberOfExtraValues)
+    {
+        this.MsgID = MsgID;
+        this.Flags = Flags;
+        this.responseCode = responseCode;
+        this.numberOfValues = numberOfValues;
+        this.numberOfAuthorities = numberOfAuthorities;
+        this.numberOfExtraValues = numberOfExtraValues;
+    }
+
+    // msgid -> 16 bits, flags -> 2 bits, rest -> 8 bits
+    // public byte[] Encode ()
+    // {
+    //     ByteBuffer bb = ByteBuffer.allocate(42);
+    //     BitSet bs = new BitSet(42);
+    //     int msgid = Integer.parseInt(this.MsgID);
+    //     bb.putInt(msgid, msgid);
+    // }
+
+    public static void main (String[] args) 
+    {
+        System.out.println((byte)255);
+    }
 }
---
+++ src/Serializer.java
@@ -0,0 +1,29 @@
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class Serializer 
+{
+    
+    public static byte[] Serialize_String (String str)
+    {
+        return str.getBytes();
+    }
+
+    public static String Deserialize_String (byte[] bytes)
+    {
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+    public static byte[] Serialize_Int (int num)
+    {
+        return ByteBuffer.allocate(4).putInt(num).array();
+    }
+
+    public static int Deserialize_Int (byte[] bytes)
+    {
+        return ByteBuffer.wrap(bytes).getInt();
+    }
+
+    
+
+}
